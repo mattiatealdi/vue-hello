45e87261f43d155b1bbebf60699ed01bc41fce2a
mattiatealdi <mattiatealdi@gmail.com>
2021-05-04 16:01:01 +0200

    push

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,15 @@ const app = new Vue({
   data:{
     message: 'Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.',
     color:'',
-    newImg: 'space_1.jpg'
+    newImg: 'assets/img/space_1.jpg'
   },
   methods:{
     changeColor(newColor){
       this.color = newColor;
-    }
-  },
-  modules:{
-    
+    },
+    changeTextClass(str) {
+      this.color = str;
+    },
   }
 
 });
